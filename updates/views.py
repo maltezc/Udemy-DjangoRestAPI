7d60959389a3d8a15ca74ca121be1e5241bef86c
cflux1990 <cflux.maltez@live.com>
2018-07-11 08:00:10 -0700

    First Commit - Chapter.Lesson 2.6 - Blank Django Project

---
+++ updates/views.py
@@ -0,0 +1,50 @@
+import json
+
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+from django.views.generic import View
+
+from cfeapi.mixins import JsonResponseMixin
+
+from .models import Update
+# Create your views here.
+# def detail_view(request):
+#     return render(request, template, {contextDictionary}) # return JSON data --> JS object notation
+    # return HttpResponse(get_template().render({}))
+
+# obj = Update.objects.get(id=1) ==> called "serialization"
+# ^used for when you want json to spit out instances for every id
+
+def json_example_view(request):
+    '''
+    URI -- for a REST API
+    GET -- Retrieve
+    '''
+
+    data = {
+        "count": 1000,
+        "content": "Some new content"
+
+
+    }
+    json_data = json.dumps(data)
+    # return JsonResponse(data)
+    return HttpResponse(json_data, content_type='application/json')
+
+
+class JsonCBV(View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+        return JsonResponse(data)
+
+class JsonCBV2(JsonResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count": 1000,
+            "content": "Some new content"
+        }
+
+        return self.render_to_json_response(data)
