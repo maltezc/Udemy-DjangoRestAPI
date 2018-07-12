66fad5f7c550f15f3054a2a458bec6a9499c2bc7
cflux1990 <cflux.maltez@live.com>
2018-07-11 20:03:26 -0700

    ch3.11 - Serialize Data

--- cfeapi/urls.py
+++ cfeapi/urls.py
@@ -17,12 +17,21 @@ from django.conf.urls import url
 from django.contrib import admin
 
 
-from updates.views import json_example_view, JsonCBV, JsonCBV2
+from updates.views import (
+        json_example_view,
+        JsonCBV,
+        JsonCBV2,
+        SerializedDetailView,
+        SerializedListView
+        )
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^json/cbv/$', JsonCBV.as_view()),
+
     url(r'^json/cbv2/$', JsonCBV2.as_view()),
     url(r'^json/example/$', json_example_view),
+    url(r'^json/serialized/detail/$', SerializedDetailView.as_view()),
+    url(r'^json/serialized/list/$', SerializedListView.as_view()),
 
 ]
