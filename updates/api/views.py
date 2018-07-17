acb9ed919ca250802e38dd2aec946eeb3a8e40af
cflux1990 <cflux.maltez@live.com>
2018-07-17 07:28:08 -0700

    ch3.23 - One Endpoint to rule them all

--- updates/api/views.py
+++ updates/api/views.py
@@ -93,21 +93,64 @@ class UpdateModelDetailAPIView(HttpResponseMixin, CSRFExemptMixin,View):
         return self.render_to_response(error_data, status=400)
 
 
+#AUTH / Permission - DJANGO REST FRAMEWORK
+#Dont use TastyPie as 3rd party authentification package
+
+
+# /api/updates/ --> 1 endpoint for all of CRUD
+
+
 class UpdateModelListAPIView(HttpResponseMixin, CSRFExemptMixin, View):
     '''
-    List View
+    List View --> retrieve
     Create View
+    Update
+    Delete
     check wikipedia for status codes
     '''
 
     is_json = True
+    queryset = None
+
+    def get_queryset(self):
+        qs = UpdateModel.objects.all()
+        self.queryset = qs
+        return qs
+
+
+    def get_object(self, id=None):
+        # try:
+        #     obj = UpdateModel.objects.get(id=id)
+        # except UpdateModel.DoesNotExist:
+        #     obj = None
+
+        '''
+            Below handles a Does Not Exist Exception too
+        '''
+
+        if id is None:
+            return None
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
 
 
     def get(self, request, *args, **kwargs):
         # handles retrieve method
-        qs = UpdateModel.objects.all()
-        json_data = qs.serialize()
-        return self.render_to_response(json_data)
+        data = json.loads(request.body)
+        passed_id = data.get("id", None)
+        if passed_id is not None:
+            obj = self.get_object(id=passed_id)
+            if obj is None:
+                error_data = json.dumps({"message": "object not found"})
+                return self.render_to_response(error_data, status=404)
+            json_data = obj.serialize()
+            return self.render_to_response(json_data)
+        else:
+            qs = self.get_queryset()
+            json_data = qs.serialize()
+            return self.render_to_response(json_data)
 
     def post(self, request, *args, **kwargs):
         # handles create method
@@ -128,8 +171,66 @@ class UpdateModelListAPIView(HttpResponseMixin, CSRFExemptMixin, View):
         data = {"message": "Not Allowed"}
         return self.render_to_response(data, status=400)
 
+    # def delete(self, request, *args, **kwargs):
+    #     # handles delete method
+    #     data = json.dumps({'message': 'You cannot delete an entire list'})
+    #     status_code = 403 # not allowed
+    #     return self.render_to_response(data, status=403)
+
+    def put(self, request, *args, **kwargs):
+        # handles update method
+        valid_json = is_json(request.body)
+        if not valid_json:
+            error_data = json.dumps({"message": "Invalid data sent"})
+            return self.render_to_response(error_data, status=400)
+        passed_data = json.loads(request.body)
+        passed_id = passed_data.get('id', None)
+        if not passed_id:
+            error_data = json.dumps({"id": "This is a required field to update an item"})
+            return self.render_to_response(error_data, status=400)
+
+        obj = self.get_object(id=passed_id)
+        if obj is None:
+            error_data = json.dumps({"message": "Object not found"})
+            return self.render_to_response(error_data, status=404)
+
+        new_data = {}
+        data = json.loads(obj.serialize())
+        for key, value in passed_data.items():
+            data[key] = value
+
+        form = UpdateModelForm(data, instance=obj)
+        if form.is_valid():
+            obj = form.save(commit=True)
+            obj_data = json.dumps(data)
+            return self.render_to_response(obj_data, status=201)
+        if form.errors:
+            data = json.dumps(form.errors)
+            return self.render_to_response(data, status=400)
+
+        json_data = json.dumps({"message": "something else"})
+        return self.render_to_response(json_data)
+
     def delete(self, request, *args, **kwargs):
         # handles delete method
-        data = json.dumps({'message': 'You cannot delete an entire list'})
-        status_code = 403 # not allowed
-        return self.render_to_response(data, status=403)
+        valid_json = is_json(request.body)
+        if not valid_json:
+            error_data = json.dumps({"message": "Invalid data sent"})
+            return self.render_to_response(error_data, status=400)
+        passed_data = json.loads(request.body)
+        passed_id = passed_data.get('id', None)
+        if not passed_id:
+            error_data = json.dumps({"id": "This is a required field to update an item"})
+
+        obj = self.get_object(id=passed_id)
+        if obj is None:
+            error_data = json.dumps({"message": "Object not found"})
+            return self.render_to_response(error_data, status=404)
+
+        deleted_, item_deleted = obj.delete()
+        print(deleted_)
+        if deleted_ == 1:
+            json_data = json.dumps({"message": "Successfully deleted."})
+            return self.render_to_response(json_data, status=200)
+        error_data = json.dumps({"message":"Could not delete item. Please try again later."})
+        return self.render_to_response(error_data, status=400)
