3b8bb81c8b82dd2cef14a5e2ac6c711522add463
cflux1990 <cflux.maltez@live.com>
2018-07-26 08:52:35 -0700

    ch4.47 - Custom Authentication View

---
+++ accounts/api/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token # would usually go in the accounts app
+# ^ passes through user's password and username for authentication when required
+
+from .views import AuthView
+
+
+# if django 2.0, this looks different V
+urlpatterns = [
+    url(r'^$', AuthView.as_view()),
+    url(r'^jwt/$', obtain_jwt_token),
+    url(r'^jwt/refresh/$', refresh_jwt_token),
+]
--- cfeapi/urls.py
+++ cfeapi/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token # would usually go in the accounts app
-# ^ passes through user's password and username for authentication when required
-
 
 from updates.views import (
         json_example_view,
@@ -33,8 +30,7 @@ from updates.views import (
 # if django 2.0, this looks different V
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/auth/jwt/$', obtain_jwt_token),
-    url(r'^api/auth/jwt/refresh/$', refresh_jwt_token),
+    url(r'^api/auth/', include('accounts.api.urls')),
     url(r'^api/status/', include('status.api.urls')),
     url(r'^api/updates/', include('updates.api.urls')),  # api/updates/ --> api/updates/1/ --> detail
 ]
