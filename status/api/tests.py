de6f885cf0cddaf22fb6fd72d18fc805b9ce48d2
cflux1990 <cflux.maltez@live.com>
2018-08-04 18:10:17 -0700

    ch4.64 - Testing with a Temporary Image

--- status/api/tests.py
+++ status/api/tests.py
@@ -1,18 +1,26 @@
+import os
+import shutil # shell utility method
+import tempfile
+from PIL import Image # pip install pillow
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.reverse import reverse as api_reverse
 from rest_framework.test import APITestCase
-
 from django.contrib.auth import get_user_model
+from django.conf import settings
 
+from rest_framework_jwt.settings import api_settings
 
 from status.models import Status
 User = get_user_model()
 
+jwt_payload_handler             = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler              = api_settings.JWT_ENCODE_HANDLER
+
 
 class StatusAPITestCase(APITestCase):
     def setUp(self):
-        user = User.objects.create(username='cfe', email='hello@cfe.com')
+        user = User.objects.create(username='testcfeuser', email='hello@cfe.com')
         user.set_password("yeahhhcfe")
         user.save()
 
@@ -24,7 +32,7 @@ class StatusAPITestCase(APITestCase):
     def status_user_token(self):
         auth_url = api_reverse('api-auth:login')
         auth_data = {
-            'username': 'cfe',
+            'username': 'testcfeuser',
             'password': 'yeahhhcfe',
         }
         auth_response = self.client.post(auth_url, auth_data, format='json')
@@ -42,6 +50,61 @@ class StatusAPITestCase(APITestCase):
         self.assertEqual(Status.objects.count(), 2)
         return response.data
 
+    def test_empty_create_item(self):
+        self.status_user_token() #calls def status usertoken above instead of having to repeat code
+        url = api_reverse('api-status:list')
+        data = {
+            'content': None,
+            'image': None,
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        return response.data
+
+    def test_status_create_with_image(self):
+        self.status_user_token()
+        url = api_reverse('api-status:list')
+        # (w, h) = (800, 1200)
+        # (255, 255, 255)
+        image_item = Image.new('RGB', (800, 1280), (0, 124, 174))
+        tmp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
+        image_item.save(tmp_file, format='JPEG')
+        with open(tmp_file.name, 'rb') as file_obj:
+            data = {
+                'content': "come cool test content",
+                'image': file_obj
+            }
+            response = self.client.post(url, data, format='multipart') # multipart allows you to handle data coming through
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+            self.assertEqual(Status.objects.count(), 2)
+        temp_img_dir = os.path.join(settings.MEDIA_ROOT, 'status', 'testcfeuser')
+        if os.path.exists(temp_img_dir):
+            shutil.rmtree(temp_img_dir)
+
+    def test_status_create_with_image_and_desc(self):
+        self.status_user_token()
+        url = api_reverse('api-status:list')
+        # (w, h) = (800, 1200)
+        # (255, 255, 255)
+        image_item = Image.new('RGB', (800, 1280), (0, 124, 174))
+        tmp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
+        image_item.save(tmp_file, format='JPEG')
+        with open(tmp_file.name, 'rb') as file_obj:
+            data = {
+                'content': None,
+                'image': file_obj
+            }
+            response = self.client.post(url, data, format='multipart') # multipart allows you to handle data coming through
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+            print(response.data)
+            img_data = response.data.get('image')
+            self.assertNotEqual(img_data, None)
+            self.assertEqual(Status.objects.count(), 2)
+        temp_img_dir = os.path.join(settings.MEDIA_ROOT, 'status', 'testcfeuser')
+        if os.path.exists(temp_img_dir):
+            shutil.rmtree(temp_img_dir)
+
+
     def test_status_create(self):
         data = self.create_item()
         data_id = data.get("id")
@@ -55,7 +118,6 @@ class StatusAPITestCase(APITestCase):
         '''
         get_response = self.client.get(rud_url, format='json')
         self.assertEqual(get_response.status_code, status.HTTP_200_OK)
-
 
 
     def test_status_update(self):
@@ -102,4 +164,22 @@ class StatusAPITestCase(APITestCase):
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_other_user_permissions_api(self):
+        data            = self.create_item()
+        data_id         = data.get("id")
+        user            = User.objects.create(username='userjmitch')
+        payload         = jwt_payload_handler(user)
+        token           = jwt_encode_handler(payload)
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
+        rud_url         = api_reverse('api-status:detail', kwargs={'id':data_id})
+        rud_data        = {
+                            'content':"smashing"
+                            }
+        get_            = self.client.get(rud_url, format='json')
+        put_            = self.client.put(rud_url, rud_data, format='json')
+        delete_         = self.client.delete(rud_url, format='json')
+        self.assertEqual(get_.status_code, status.HTTP_200_OK)
+        self.assertEqual(put_.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(delete_.status_code, status.HTTP_403_FORBIDDEN)
 
